098968471d598dc04ff5c5d7ef8e8a843ad90e51
Agrima Sharma <agrimasharma411@gmail.com>
2025-07-24 21:20:07 +0530

    Enum added

---
+++ IOS Notes/IOS Notes/Important Topics/Important Topics/Enum/Enum.swift
@@ -0,0 +1,73 @@
+/*
+ 
+ 
+ 
+ |-------------------------------|
+ |                               |
+ |   🚀        Enum              |
+ |                               |
+ |-------------------------------|
+ 
+ 
+ 
+ 
+ ✅ What is enum in Swift?
+ 
+ enum is   a value type used to define a group of related values       in a type-safe way. It can also have associated             values, raw values, and can include methods and             computed properties.
+                
+ 
+ 
+ 
+ ✅ Example 1: Simple Enum
+ 
+ enum Direction {
+     case north, south, east, west
+ }
+ ----------------------------------------------------------
+ 
+ ✅ Example 2: Raw Values
+ 
+ enum Status: Int {
+     case success = 1
+     case failure = 0
+ }
+ 
+ ----------------------------------------------------------
+ 
+ 
+ ✅ Example 3: Associated Values
+
+ enum Media {
+     case photo(String)
+     case video(String, Int)
+ }
+ 
+ ----------------------------------------------------------
+ 
+ 
+ ✅ Example 4: With Method
+ 
+ enum Direction {
+     case north, south
+     func move() {
+         print("Moving \(self)")
+     }
+ }
+ 
+ ----------------------------------------------------------
+ 
+ 
+ ✅ Example 5: With Property
+ 
+ enum Status: Int {
+     case success = 1
+     case failure = 0
+
+     var message: String {
+         self == .success ? "Success" : "Failure"
+     }
+ }
+ ----------------------------------------------------------
+  
+ 
+ */
---
+++ IOS Notes/IOS Notes/Important Topics/Important Topics/Extension/extension class-struct-enum-protocol.swift
@@ -0,0 +1,41 @@
+/*
+ 
+ |-------------------------------|
+ |                               |
+ |         🚀  Extension         |
+ |                               |
+ |-------------------------------|
+ 
+    ✅ What is extension in Swift?
+ 
+ extension is used to add new functionality to an existing                class, struct, enum, or protocol — without                  modifying its original code.
+ 
+ ------------------------------------------------------------
+ ✅ Why use it?
+ 
+ To organize code better
+
+ To separate functionality
+
+ To follow clean code practices
+ 
+ ------------------------------------------------------------
+ 
+
+ 
+ ✅ Simple Example – Add method to Int:
+ 
+ 
+ extension Int {
+     func square() -> Int {
+         return self * self
+     }
+ }
+
+ let num: Int = 4
+ print(num.square())  // Output: 16 ✅
+ 
+ 
+ 
+ 
+ */
